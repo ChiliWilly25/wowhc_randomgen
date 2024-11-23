70d5c28ab7d74f56eaa66fcf86dd796d3946c379
Chiliwilly25 <chacagames25@gmail.com>
2024-11-23 18:09:00 -0300

    Updated app to use Render-provided PORT

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from urllib.parse import urlencode
 import random
+import os
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -299,4 +300,6 @@ Professions: {first_profession} and {second_profession}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the PORT from the environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
